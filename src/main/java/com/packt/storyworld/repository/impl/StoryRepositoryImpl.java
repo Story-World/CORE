72869a4add4766e614198f1c1f5d098019fb470a
anik93 <proxianik@gmail.com>
2017-02-21 19:52:19 +0100

    @Repository added

--- src/main/java/com/packt/storyworld/repository/impl/StoryRepositoryImpl.java
+++ src/main/java/com/packt/storyworld/repository/impl/StoryRepositoryImpl.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.packt.storyworld.domain.mongo.Story;
+import com.packt.storyworld.repository.StoryRepository;
 
-public class StoryRepositoryImpl {
+@Repository
+public class StoryRepositoryImpl implements StoryRepository{
 
 	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(StoryRepositoryImpl.class);
